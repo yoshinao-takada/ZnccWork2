b4591397d26f0aa2cee89bb34adaa34c07295c50
Yoshinao Takada <yoshinao_takada2002@yahoo.co.jp>
2021-12-11 09:29:05 +0000

    finished

--- PerfTest/PerfTestMain.cpp
+++ PerfTest/PerfTestMain.cpp
@@ -14,21 +14,21 @@ int main()
             fprintf(stderr, "%s,%d,fail in PerfTestCGrid()\n", __FUNCTION__, __LINE__);
             break;
         }
-        // if (EXIT_SUCCESS != (err = PerfTestCWithoutCostMap()))
-        // {
-        //     fprintf(stderr, "%s,%d,fail in PerfTestCWithoutCostMap()\n", __FUNCTION__, __LINE__);
-        //     break;
-        // }
-        // if (EXIT_SUCCESS != (err = PerfTestC()))
-        // {
-        //     fprintf(stderr, "%s,%d,fail in PerfTestC()\n", __FUNCTION__, __LINE__);
-        //     break;
-        // }
-        // if (EXIT_SUCCESS != (err = PerfTestISPC()))
-        // {
-        //     fprintf(stderr, "%s,%d,fail in PerfTestISPC()\n", __FUNCTION__, __LINE__);
-        //     break;
-        // }
+        if (EXIT_SUCCESS != (err = PerfTestCWithoutCostMap()))
+        {
+            fprintf(stderr, "%s,%d,fail in PerfTestCWithoutCostMap()\n", __FUNCTION__, __LINE__);
+            break;
+        }
+        if (EXIT_SUCCESS != (err = PerfTestC()))
+        {
+            fprintf(stderr, "%s,%d,fail in PerfTestC()\n", __FUNCTION__, __LINE__);
+            break;
+        }
+        if (EXIT_SUCCESS != (err = PerfTestISPC()))
+        {
+            fprintf(stderr, "%s,%d,fail in PerfTestISPC()\n", __FUNCTION__, __LINE__);
+            break;
+        }
     } while (0);
     return err;
 }
